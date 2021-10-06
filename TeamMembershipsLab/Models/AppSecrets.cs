3b4ac6ce08370c5d60e0a5f154b6b6c31874d480
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-10-05 20:06:50 -0400

    added comments to AppSecrets class.

--- TeamMembershipsLab/Models/AppSecrets.cs
+++ TeamMembershipsLab/Models/AppSecrets.cs
@@ -5,9 +5,21 @@ using System.Threading.Tasks;
 
 namespace TeamMembershipsLab.Models
 {
+    /// <summary>
+    /// AppSecret class.
+    /// </summary>
     public class AppSecrets
     {
+        /// <summary>
+        /// Gets or sets Adminpass.
+        /// <value>Admin's password.</value>
+        /// </summary>
         public string AdminPass { get; set; }
+
+        /// <summary>
+        /// Gets or sets MemberPass.
+        /// <value>Member's password.</value>
+        /// </summary>
         public string MemberPass { get; set; }
     }
 }
